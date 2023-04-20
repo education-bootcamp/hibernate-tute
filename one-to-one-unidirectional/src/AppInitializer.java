98f14e3f32830dff3a44daeedffc8ad161ff4278
ozen-hub <info.ozenhub@gmail.com>
2023-04-20 20:31:49 +0530

    init

---
+++ one-to-one-unidirectional/src/AppInitializer.java
@@ -0,0 +1,16 @@
+import entity.Laptop;
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Arrays;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+        }
+
+    }
+
+}
---
+++ one-to-one-unidirectional/src/entity/Laptop.java
@@ -0,0 +1,6 @@
+package entity;
+
+public class Laptop {
+    private long laptopId;
+    private String brand;
+}
