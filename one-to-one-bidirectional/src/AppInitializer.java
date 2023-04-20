06ca451e087ce0da010e12f0b2d6621006e9e2ad
ozen-hub <info.ozenhub@gmail.com>
2023-04-20 21:09:21 +0530

    3

---
+++ one-to-one-bidirectional/src/AppInitializer.java
@@ -0,0 +1,21 @@
+import entity.Laptop;
+import entity.Student;
+import org.hibernate.Session;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            //=-------------
+            Laptop laptop = new Laptop();
+            laptop.setBrand("Lenovo");
+            session.save(laptop);
+            //------------
+            Student student = new Student();
+            student.setLaptop(laptop);
+            student.setStudentName("Nimal");
+            session.save(student);
+        }
+
+    }
+
+}
---
+++ one-to-one-bidirectional/src/entity/Student.java
@@ -0,0 +1,50 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity(name = "student_table")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private long studentId;
+    private String studentName;
+
+    //--------------mapping----------------
+    @OneToOne
+    @JoinColumn(name = "laptop_id")
+    private Laptop laptop;
+    //----------------mapping--------------
+
+    public Student() {
+    }
+
+    public Student(long studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
